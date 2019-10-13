276d7b349899c37c435a0b0430c63ac22ede78f1
mkuziv <mykhaylokuziv@gmail.com>
2019-10-13 21:51:50 +0300

    add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,29 @@
-module.exports = function multiply(first, second) {
-  // your solution
+module.exports = function multiply(num1, num2) {
+
+  num1 = num1.split('').reverse();
+  num2 = num2.split('').reverse();
+  let result = [];
+
+  for (var i = 0; num1[i] >= 0; i++) {
+    for (var j = 0; num2[j] >= 0; j++) {
+      if (!result[i + j]) {
+        result[i + j] = 0;
+      }
+
+      result[i + j] += num1[i] * num2[j];
+    }
+  }
+
+  for (var i = 0; result[i] >= 0; i++) {
+    if (result[i] >= 10) {
+      if (!result[i + 1]) {
+        result[i + 1] = 0;
+      }
+
+      result[i + 1] += parseInt(result[i] / 10);
+      result[i] %= 10;
+    }
+  }
+
+  return result.reverse().join('');
 }
